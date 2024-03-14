330956cf58f087ffbbb648e3c7d0bc2b220e860a
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2024-03-14 13:43:08 +0200

    add sections

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,4 +2,28 @@ const burgerBtn = document.querySelector('.menuBtns');
 const headerWrapperMobile = document.querySelector('.header__container-bottom-wrapper')
 burgerBtn.addEventListener('click', () =>{
     headerWrapperMobile.classList.toggle('open');
-})
+});
+const reviewsSlider = new Swiper('.reviewsSlider', {
+    slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation:{
+      nextEl: '.reviews__container-top-arrow-next',
+      prevEl: '.reviews__container-top-arrow-prev',
+      
+  },
+  // Responsive breakpoints
+  breakpoints: {
+   
+    
+    // when window width is >= 640px
+    767: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    }
+  }
+
+  });
